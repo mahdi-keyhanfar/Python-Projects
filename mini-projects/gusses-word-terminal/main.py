0da10849901049c4c4f0cd4c2eaeeade2c2a6ca0
Mahdi Keyhanfar <kiyanknf@gmail.com>
2025-08-09 13:44:21 +0330

    Add gusses-word-terminal.py

---
+++ mini-projects/gusses-word-terminal/main.py
@@ -0,0 +1,24 @@
+import random
+time_try = 0 
+word = ['apple','world','ball','book','bag']
+
+random_chose = random.choice(word)
+data1 = ["-"] *len(random_chose)
+print(data1)
+while time_try < 6:
+    
+    data2 = input("Enter Your Ges: ")
+    if data2 in random_chose :
+        for i in range(len(random_chose)):
+            if random_chose[i] == data2:
+                data1[i] = data2
+                print(data1)
+    else:
+        time_try+=1
+        print(f"The number of your mistakes: {time_try}/6")
+
+    if time_try == 6:
+        break
+
+print(data1)
+print("Answer:", random_chose) 
