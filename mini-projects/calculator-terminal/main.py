c841306bedee243db4eeb11f9a7342898e789551
Mahdi Keyhanfar <kiyanknf@gmail.com>
2025-08-09 12:21:06 +0330

    Add calculator-terminal.py

--- mini-projects/calculator-terminal/main.py
+++ mini-projects/calculator-terminal/main.py
@@ -4,9 +4,11 @@ data3 = float(input("Enter Your Number2 : "))
 
 if data2 == "+":
     print(f"The sum of your numbers ==> {data1+data3}")
-if data2 == "-":
+elif data2 == "-":
     print(f"The minus of your numbers ==> {data1-data3}")
-if data2 == "*":
+elif data2 == "*":
     print(f"The times of your numbers ==> {data1*data3}")
-if data2 == "/":
+elif data2 == "/":
     print(f"The divided of your numbers ==> {data1/data3}")
+else:
+    print("Invalid Input")
