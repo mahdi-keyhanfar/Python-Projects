5508e652c7144a08877cf9afc4ddc7b62e2b7137
Mahdi Keyhanfar <kiyanknf@gmail.com>
2025-08-18 21:01:27 +0330

    Add gusses-number.py

--- mini-projects/gusses-number/main.py
+++ mini-projects/gusses-number/main.py
@@ -6,7 +6,7 @@ choice = random.randint(1,10)
 while guess_number != choice:
     guess_number = int(input("Gusses a number: "))
     if guess_number == choice:
-        print("You win 🏆")
+        print("\033[92mYou win 🏆\033[0m")
     else:
         counter_gusess += 1
-        print(f"Try again🥱 \nThe number of gusses:{counter_gusess}")
+        print(f"Try again🥱 \n\033[91mThe number of gusses:{counter_gusess}\033[0m")
